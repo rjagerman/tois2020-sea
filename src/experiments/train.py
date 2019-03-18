a515cbd2b9ea46d1689442997033da06fe5f749a
Rolf Jagerman <rjagerman@gmail.com>
2019-03-18 14:22:05 +0100

    Make cachedir configurable

--- src/experiments/train.py
+++ src/experiments/train.py
@@ -15,6 +15,7 @@ def main():
     cli_parser = ArgumentParser()
     cli_parser.add_argument("-p", "--parallel", type=int, default=1)
     cli_parser.add_argument("-c", "--config", type=str, required=True)
+    cli_parser.add_argument("--cache", type=str, default="cache")
     args = cli_parser.parse_args()
 
     parser = ArgumentParser()
@@ -59,7 +60,7 @@ def main():
     #     f"--train_path {train} --test_path {test} --seed {seed} --strategy epsgreedy --lr 1e-6 --eval_scale {scale} --evaluations 5"
     # ]
 
-    with TaskExecutor(max_workers=args.parallel, memory=Memory("cache", compress=6)):
+    with TaskExecutor(max_workers=args.parallel, memory=Memory(args.cache, compress=6)):
         results = [run_experiment(config) for config in configs]
     
     for result, config in zip(results, configs):
