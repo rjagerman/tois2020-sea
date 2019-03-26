c8781dd00e717a3b4d7187829220612e0674f363
Rolf Jagerman <rjagerman@gmail.com>
2019-03-26 18:02:48 +0100

    Add more sparse functionality

--- src/experiments/sparse.py
+++ src/experiments/sparse.py
@@ -1,14 +1,15 @@
 import numpy as np
 import numba
 from rulpy.array import GrowingArray, GrowingArrayList
+from rulpy.array.growing_array import GrowingArrayF64, GrowingArrayI32
 
 
 @numba.jitclass([
     ('data', numba.float64[:]),
     ('indices', numba.int32[:]),
     ('indptr', numba.int32[:]),
-    ('nnz', numba.int32),
-    ('shape', numba.types.UniTuple(numba.int32, 2))
+    ('nnz', numba.int64),
+    ('shape', numba.types.UniTuple(numba.int64, 2))
 ])
 class _SparseMatrix:
     def __init__(self, data, indices, indptr, nnz, shape):
@@ -30,6 +31,46 @@ class _SparseMatrix:
             (self.shape[1],)
         )
 
+    def from_dense(self, matrix, transpose=False):
+        _data = GrowingArrayF64(16)
+        _indices = GrowingArrayI32(16)
+        _indptr = GrowingArrayI32(16)
+        _nnz = 0
+        _indptr.append(0)
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                if matrix[i, j] != 0.0:
+                    _data.append(matrix[i, j])
+                    _indices.append(j)
+                    _nnz += 1
+            _indptr.append(_nnz)
+        self.data = np.copy(_data.array)
+        self.indices = np.copy(_indices.array)
+        self.indptr = np.copy(_indptr.array)
+        self.nnz = _nnz
+        self.shape = matrix.shape
+    
+    def to_dense(self, transpose=False):
+        shape = (self.shape[0], self.shape[1])
+        if transpose:
+            shape = (self.shape[1], self.shape[0])
+        out = np.zeros((self.shape[1], self.shape[0]), dtype=np.float64)
+        for row in range(self.shape[0]):
+            start = self.indptr[row]
+            end = self.indptr[row + 1]
+            for i in range(start, end):
+                i1, i2 = row, self.indices[i]
+                if transpose:
+                    i1, i2 = i2, i1
+                out[i1, i2] = self.data[i]
+        return out
+
+    def clear(self):
+        self.data = np.zeros(1, dtype=np.float64)
+        self.indices = np.zeros(1, dtype=np.int32)
+        self.indptr = np.zeros(2, dtype=np.int32)
+        self.nnz = 0
+
 
 def __matrix_getstate(self):
     return {
@@ -50,22 +91,37 @@ def __matrix_setstate(self, state):
 
 
 def __matrix_reduce(self):
-    return (_SparseMatrix, (self.data, self.indices, self.indptr, self.nnz, self.shape))
+    return (SparseMatrix, (self.data, self.indices, self.indptr, self.nnz, self.shape))
+
+
+def __matrix_deepcopy(self):
+    return SparseMatrix(
+        np.copy(self.data),
+        np.copy(self.indices),
+        np.copy(self.indptr),
+        self.nnz,
+        self.shape
+    )
 
 
 def from_scipy(matrix, min_d=0):
-    out = _SparseMatrix(matrix.data, matrix.indices, matrix.indptr, matrix.nnz, (matrix.shape[0], max(min_d, matrix.shape[1])))
+    return SparseMatrix(matrix.data, matrix.indices, matrix.indptr, matrix.nnz, (matrix.shape[0], max(min_d, matrix.shape[1])))
+
+
+def SparseMatrix(data, indices, indptr, nnz, shape):
+    out = _SparseMatrix(data, indices, indptr, nnz, shape)
     setattr(out.__class__, '__getstate__', __matrix_getstate)
     setattr(out.__class__, '__setstate__', __matrix_setstate)
     setattr(out.__class__, '__reduce__', __matrix_reduce)
+    setattr(out.__class__, '__deepcopy__', __matrix_deepcopy)
     return out
 
 
 @numba.jitclass([
     ('data', numba.float64[:]),
     ('indices', numba.int32[:]),
-    ('nnz', numba.int32),
-    ('shape', numba.types.UniTuple(numba.int32, 1))
+    ('nnz', numba.int64),
+    ('shape', numba.types.UniTuple(numba.int64, 1))
 ])
 class _SparseVector:
     def __init__(self, data, indices, nnz, shape):
@@ -121,8 +177,8 @@ def _sparse_vector_dot_vector(sv, other):
 @numba.jitclass([
     ('data', numba.typeof(GrowingArrayList(dtype=numba.float64))),
     ('indices', numba.typeof(GrowingArrayList(dtype=numba.int32))),
-    ('nnzs', numba.typeof(GrowingArray(dtype=numba.int32))),
-    ('shape', numba.types.UniTuple(numba.int32, 1))
+    ('nnzs', numba.typeof(GrowingArray(dtype=numba.int64))),
+    ('shape', numba.types.UniTuple(numba.int64, 1))
 ])
 class _SparseVectorList:
     def __init__(self, data, indices, nnzs, shape):
@@ -181,7 +237,7 @@ def __vectorlist_deepcopy(self):
 def SparseVectorList(shape, capacity=16):
     data = GrowingArrayList(capacity, dtype=numba.float64)
     indices = GrowingArrayList(capacity, dtype=numba.int32)
-    nnzs = GrowingArray(capacity, dtype=numba.int32)
+    nnzs = GrowingArray(capacity, dtype=numba.int64)
     out = _SparseVectorList(data, indices, nnzs, shape)
     setattr(out.__class__, '__getstate__', __vectorlist_getstate)
     setattr(out.__class__, '__setstate__', __vectorlist_setstate)
